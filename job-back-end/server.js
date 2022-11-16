3d0fe0b697b4320e8628d90359e7f20ffc6cf8f9
Manan Bhargava <manan.bhargava@berkeley.edu>
2022-11-15 19:46:20 -0800

    created front end and back end scaffolding

---
+++ job-back-end/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const axios = require("axios");
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+/* TODO: Replace this with your own API key */
+const API_KEY = "";
+
+// work on webscraping
+
+
+// work on API calls
+
