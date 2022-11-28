a20abb499f8a1a78305ab437c94ca13d1482263a
Darleen Salazar <darleensalazar@darleens-mbp.lan>
2022-11-27 19:50:56 -0800

    initial commit

---
+++ job-front-end/navigation-bar/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Teams from './pages/team';
+import SignUp from './pages/signup';
+  
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/team' component={Teams} />
+        <Route path='/sign-up' component={SignUp} />
+      </Routes>
+    </Router>
+  );
+}
+  
+export default App;
