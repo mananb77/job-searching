1c255f4cb4a1a6799bed4586695bf8f983abbc08
Mohammed Ali Abed <mohammedabedphs@gmail.com>
2022-12-01 15:13:51 -0800

    Darleens table included

--- job-front-end/src/components/Header.js
+++ job-front-end/src/components/Header.js
@@ -24,12 +24,19 @@ const Header = () => {
             your needs.
           </h3>
           <br></br>
-          <p>Our platform constantly searches the web for current openings across the realm of tech.<br></br>
-          By doing so, we are able to keep users updated (daily) with internships and full time offerings.<br></br>
-          ExTech allows for users to search a job description, title, location, and/or name and they will be <br></br>
-          greeted with various job opportunities of the same nature or jobs that satisfy the user's search request.
-          <br></br><br></br> 
-          Try it out below!</p>
+          <p>
+            Our platform constantly searches the web for current openings across
+            the realm of tech.<br></br>
+            By doing so, we are able to keep users updated (daily) with
+            internships and full time offerings.<br></br>
+            ExTech allows for users to search a job description, title,
+            location, and/or name and they will be <br></br>
+            greeted with various job opportunities of the same nature or jobs
+            that satisfy the user's search request.
+            <br></br>
+            <br></br>
+            Try it out below!
+          </p>
         </center>
       </div>
     </div>
