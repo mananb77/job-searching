b5baf1799513882e4a0b86e75b4bfeb79ed27702
Mohammed Ali Abed <mohammedabedphs@gmail.com>
2022-12-01 15:12:34 -0800

    Code cleaned & edited; footer and header complete

--- job-front-end/src/components/Header.js
+++ job-front-end/src/components/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
           By doing so, we are able to keep users updated (daily) with internships and full time offerings.<br></br>
           ExTech allows for users to search a job description, title, location, and/or name and they will be <br></br>
           greeted with various job opportunities of the same nature or jobs that satisfy the user's search request.
-          <br></br><br></br>
+          <br></br><br></br> 
           Try it out below!</p>
         </center>
       </div>
