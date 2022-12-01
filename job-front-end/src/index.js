c1253fc2fb09909db1214e21ae3474393c4c0eed
Mohammed Ali Abed (he/him) <100828652+moatcal@users.noreply.github.com>
2022-12-01 14:40:26 -0800

    Update index.js

--- job-front-end/src/index.js
+++ job-front-end/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import 'bootstrap/dist/css/bootstrap.css';
-
-
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,9 +11,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-
+// For VIVID.LOL
+if (
+  typeof window !== "undefined" &&
+  process.env.NODE_ENV === "development"
+  // && /VIVID_ENABLED=true/.test(document.cookie)
+) {
+  import("vivid-studio").then((v) => v.run());
+  import("vivid-studio/style.css");
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
